1a08bcc84dd74f1fdb9a13c0e3a717b6c07c17f4
Louis Pillon <louisgzan.pro@gmail.com>
2021-05-10 10:09:06 +0200

    wip : categories

---
+++ src/components/Categories/Boissons.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Boissons = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Boissons
---
+++ src/components/Categories/Plats.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Plats = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Plats
